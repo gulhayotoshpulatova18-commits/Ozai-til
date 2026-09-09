700e98cd2a6c0f0686066a64eac0d9b7f9b82f6f
gulhayotoshpulatova18-commits <gulhayotoshpulatova18@gmail.com>
2026-09-09 23:28:04 +0500

    Create test_main.py

---
+++ test_main.py
@@ -0,0 +1,8 @@
+from main import matnni_tekshirish
+
+
+def test_matnni_tekshirish():
+    natija = matnni_tekshirish("Men o'zbek tilini yaxshi ko'raman.")
+    assert isinstance(natija, dict)
+    assert "so'zlar_soni" in natija
+    assert "gaplar_soni" in natija
